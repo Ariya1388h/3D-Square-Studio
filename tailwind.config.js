dd9821da039ffef0e8837381f832db9beae78eb2
Ariya1388H <aria.hasanzade8@gmail.com>
2024-11-08 01:39:28 +0330

    Hero & About Section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./source/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'tomorrow': ['Tomorrow']
+      }
+    },
+  },
+  plugins: [],
+}
+
